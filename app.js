b20e12d5e32397d92e130cb36a4b4340d4c674b4
RobbyWH <robby.widyahartono@gmail.com>
2017-03-24 17:59:01 +0700

    Finish interface

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
 
+profile = require('./profile.js')
 
-const https = require("https")
-const options = {
-  hostname: 'api.github.com',
-  port: 443,
-  path: '/users/robbywh',
-  method: 'GET',
-  headers: {
-    'user-agent': 'nodejs'
-  }
-}
-
-// TODO: [x] Read the data
-let request = https.request(options, (response) => {
-  let body = ''
-  response.on('data', (data) => {
-    body = body + data
-  })
-  response.on('end', () => {
-    // TODO: Parse the data
-    // Convert String to JSON
-    let profile = JSON.parse(body)
-    console.log(profile)
-  })
-})
-
-request.end()
-
-request.on('error', (e) => {
-  console.error(e)
-})
+let profiles = process.argv.slice(2)
+profiles.map(user => profile.get(user))
+//let profiles = ['robbywh', 'yofri', 'rizafahmi']
