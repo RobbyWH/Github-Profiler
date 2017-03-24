889500274c4fa57793e7f9d42b8d74b3acf5ed67
karir <karir@karirs-iMac.local>
2017-03-24 14:59:43 +0700

    Add hacktiv8

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// Problem: We need a simple to look at Github profile
-// Solution: Use NodeJS to connect to GitHub API to
-// get profile info and print out to the console
-// TODO: Connect to GitHub API
+
+
 const https = require("https")
 const options = {
   hostname: 'api.github.com',
@@ -20,6 +18,3 @@ request.end()
 request.on('error', (e) => {
   console.error(e)
 })
-// TODO: Read the data
-// TODO: Parse the data
-// TODO: Print the data out
