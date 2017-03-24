86188a7eed483cd127b643df473936ea03959b83
RobbyWH <robby.widyahartono@gmail.com>
2017-03-24 17:25:52 +0700

    Finish parse data

--- app.js
+++ app.js
@@ -10,11 +10,23 @@ const options = {
     'user-agent': 'nodejs'
   }
 }
+
+// TODO: [x] Read the data
 let request = https.request(options, (response) => {
-  console.log("Got response: ", response);
+  let body = ''
+  response.on('data', (data) => {
+    body = body + data
+  })
+  response.on('end', () => {
+    // TODO: Parse the data
+    // Convert String to JSON
+    let profile = JSON.parse(body)
+    console.log(profile)
+  })
 })
 
 request.end()
+
 request.on('error', (e) => {
   console.error(e)
 })
